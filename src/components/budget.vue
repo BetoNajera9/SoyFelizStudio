43da7506c4187dc558cc3bbe1652ad1e549f8243
BetoNajera <ing.betonajera@gmail.com>
2023-08-01 15:11:24 -0600

    :sparkles: Feat: Add form verification
    Add form verification

--- src/components/budget.vue
+++ src/components/budget.vue
@@ -3,45 +3,61 @@
 		<span class="text-6xl">
 			CO<span class="text-red">T</span>IZA TU TATUAJE
 		</span>
-		<form class="grid grid-cols-2 mr-5" action="">
+		<form class="grid grid-cols-2 mr-5" onsubmit="return false">
 			<div class="flex flex-col">
 				<span class="mt-4">Nombre completo</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="name" />
 				<span class="mt-4">E-mail</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="email" />
 				<span class="mt-4">Celular</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="phone" />
 				<span>Fecha de nacimiento</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="birthday" />
 				<span class="mt-4">Dirección</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="address" />
 			</div>
 			<div class="flex flex-col ml-5">
 				<span class="mt-4">Zona del cuerpo donde quieres tu tatuaje</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="zone" />
 				<span class="mt-4">Tamaño (cm x cm)</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="size" />
 				<span class="mt-4">Describenos tu idea</span>
-				<input type="text" :class="inputStyle" />
+				<input type="text" :class="inputStyle" v-model="idea" />
 				<span class="mt-4">Adjunta imagenes de referencia</span>
 				<input
-					type="file"
 					class="file:border-red file:bg-white file:px-3 file:py-2"
+					type="file"
 				/>
 				<div class="flex items-center mt-10">
-					<input type="checkbox" class="outline-red otline-1" />
-					<span class="text-red ml-2 hover:underline hover:cursor-pointer"
+					<input
+						class="outline-red otline-1"
+						type="checkbox"
+						v-model="check"
+						value="privacy"
+					/>
+					<span
+						@click="$emit('closen', 'privacy')"
+						class="text-red ml-2 hover:underline hover:cursor-pointer"
 						>He leido aviso de privacidad</span
 					>
 				</div>
 				<div class="flex items-center">
-					<input type="checkbox" class="outline-red otline-1" />
-					<span class="text-red ml-2 hover:underline hover:cursor-pointer"
+					<input
+						class="outline-red otline-1"
+						type="checkbox"
+						v-model="check"
+						value="conditions"
+					/>
+					<span
+						@click="$emit('closen', 'conditions')"
+						class="text-red ml-2 hover:underline hover:cursor-pointer"
 						>He leido terminos y condiciones</span
 					>
 				</div>
 				<div class="flex mt-10">
-					<button class="bg-black text-white px-10 py-1">ENVIAR</button>
+					<button class="bg-black text-white px-10 py-1" @click="send">
+						ENVIAR
+					</button>
 				</div>
 			</div>
 		</form>
@@ -49,12 +65,51 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
 	setup() {
 		const inputStyle = 'border-[1px] border-red h-10'
 
+		const birthday = ref('')
+		const address = ref('')
+		const email = ref('')
+		const phone = ref('')
+		const name = ref('')
+		const zone = ref('')
+		const size = ref('')
+		const idea = ref('')
+
+		const check = ref([])
+
+		const send = () => {
+			if (
+				name.value.trim() === '' ||
+				email.value.trim() === '' ||
+				phone.value.trim() === '' ||
+				birthday.value.trim() === '' ||
+				address.value.trim() === '' ||
+				zone.value.trim() === '' ||
+				size.value.trim() === '' ||
+				idea.value.trim() === '' ||
+				check.value.length < 2
+			) {
+				alert('Se necesita llenar todos los campos')
+			} else alert('Send email')
+		}
+
 		return {
 			inputStyle,
+			birthday,
+			address,
+			email,
+			phone,
+			check,
+			name,
+			zone,
+			size,
+			idea,
+			send,
 		}
 	},
 }
