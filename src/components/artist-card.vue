a7da7cf861c09f99f38dc1efb285f8d78387b9cb
BetoNajera <ing.betonajera@gmail.com>
2023-08-01 10:59:45 -0600

    :sparkles: Feat: Add artist component with his artist cards
    Add artist component with his artist cards

---
+++ src/components/artist-card.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="flex flex-col mx-10">
+		<div class="">
+			<img src="/src/assets/images/desktop/artists/ARIO.png" alt="" />
+		</div>
+		<div class="flex items-center mt-5">
+			<span class="h-auto w-auto">
+				<img src="/src/assets/images/desktop/menu/spark.png" alt="" />
+			</span>
+			<span class="text-4xl h-auto">Pablo</span>
+		</div>
+		<div class="mr-auto pl-[25px]">
+			<span>@soyfeliz.pablodiaz</span>
+		</div>
+		<p class="text-left mt-7 pl-[25px]">
+			Creador del estudio de tatuajes Soy Feliz Studio en la Ciudad de México.
+			Después de una trayectoria de Diseño Gráfico, Arte y Producción, lleva su
+			talento al mundo del tatuaje a partir del 2010. Su trabajo se especializa
+			en el trazo fino y meticuloso, geometría y color; de inspira en la
+			naturaleza, la geometría sagrada y el universo.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {},
+}
+</script>
---
+++ src/components/artists.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="my-5">
+		<span class="text-6xl mx-10">
+			AR<span class="text-red">T</span>IS<span class="text-red">T</span>AS
+		</span>
+		<carousel v-bind="settings" :breakpoints="breakpoints">
+			<slide v-for="slide in 10" :key="slide">
+				<artist-card />
+			</slide>
+
+			<template #addons>
+				<navigation />
+				<pagination />
+			</template>
+		</carousel>
+	</div>
+</template>
+
+<script>
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+import ArtistCard from './artist-card.vue'
+
+import 'vue3-carousel/dist/carousel.css'
+
+export default {
+	components: {
+		Pagination,
+		Navigation,
+		ArtistCard,
+		Carousel,
+		Slide,
+	},
+
+	setup() {
+		const settings = {
+			itemsToShow: 1,
+			snapAlign: 'center',
+		}
+		// breakpoints are mobile first
+		// any settings not specified will fallback to the carousel settings
+		const breakpoints = {
+			// 700px and up
+			700: {
+				itemsToShow: 3.5,
+				snapAlign: 'center',
+			},
+			// 1024 and up
+			1024: {
+				itemsToShow: 3,
+				snapAlign: 'start',
+			},
+		}
+
+		return {
+			breakpoints,
+			settings,
+		}
+	},
+}
+</script>
