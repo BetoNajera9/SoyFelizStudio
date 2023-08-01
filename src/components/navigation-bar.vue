c610c4cfa2b4ab3a712a94c742b7b1756ecccca7
BetoNajera <ing.betonajera@gmail.com>
2023-08-01 10:58:10 -0600

    :zap: Perf: Update navigation bar with sticky position
    Update navigation bar with sticky position

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -1,22 +1,24 @@
 <template>
-	<nav class="max-w-5xl mx-auto h-[10%] flex justify-around items-center pt-2">
-		<img
-			src="/src/assets/images/desktop/menu/happy-index.png"
-			alt="soy feliz studio"
-		/>
-		<span>LLEVAMOS EL ARTE EN LA PIEL</span>
-		<img
-			class="scale-[.30]"
-			src="/src/assets/images/desktop/menu/spark.png"
-			alt="spark"
-		/>
-		<div class="flex justify-around basis-3/5">
-			<span class="cursor-pointer">INICIO</span>
-			<span class="cursor-pointer">NOSOTROS</span>
-			<span class="cursor-pointer">GALERIA</span>
-			<span class="cursor-pointer">ARTISTAS</span>
-			<span class="cursor-pointer">COTIZA</span>
-			<span class="cursor-pointer">CONTACTO</span>
+	<nav class="bg-white z-[100] h-[10%] sticky top-0">
+		<div class="max-w-5xl mx-auto flex justify-around items-center pt-2">
+			<img
+				src="/src/assets/images/desktop/menu/happy-index.png"
+				alt="soy feliz studio"
+			/>
+			<span>LLEVAMOS EL ARTE EN LA PIEL</span>
+			<img
+				class="scale-[.30]"
+				src="/src/assets/images/desktop/menu/spark.png"
+				alt="spark"
+			/>
+			<div class="flex justify-around basis-3/5">
+				<span class="cursor-pointer">INICIO</span>
+				<span class="cursor-pointer">NOSOTROS</span>
+				<span class="cursor-pointer">GALERIA</span>
+				<span class="cursor-pointer">ARTISTAS</span>
+				<span class="cursor-pointer">COTIZA</span>
+				<span class="cursor-pointer">CONTACTO</span>
+			</div>
 		</div>
 	</nav>
 </template>
